11e03080e9ca286596cf36e69c62277fb7dd663e
ozamiatin <ozamiatin@mirantis.com>
2016-08-01 01:08:40 +0300

    Install deps to a separate script

--- install_deps_ubuntu.sh
+++ install_deps_ubuntu.sh
@@ -4,9 +4,3 @@ sudo apt-get -y install cmake
 
 echo "Installing BOOST libraries"
 sudo apt-get -y install libboost-dev libboost-program-options-dev
-
-mkdir build-Debug
-cd build-Debug
-
-cmake ../src -DCMAKE_BUILD_TYPE=Debug
-make -j
