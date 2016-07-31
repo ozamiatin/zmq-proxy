4bed9aa6ea16aedd07a8df3f47b68eb25e794340
ozamiatin <ozamiatin@mirantis.com>
2016-07-31 21:05:01 +0300

    no install zmq

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -4,8 +4,8 @@
 #echo "Installing BOOST libraries"
 #sudo apt-get install -qq --force-yes libboost-dev libboost-program-options-dev
 
-echo "Installing ZeroMQ"
-sudo apt-get install -qq --force-yes libzmq5-dev
+#echo "Installing ZeroMQ"
+#sudo apt-get install -qq --force-yes libzmq5-dev
 
 mkdir build-Debug
 cd build-Debug
