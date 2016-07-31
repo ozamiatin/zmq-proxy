a232e27027946328f48b682b0fdce5024d7f8a08
ozamiatin <ozamiatin@mirantis.com>
2016-08-01 01:02:20 +0300

    Force yes

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,6 +1,6 @@
 echo "Installing cmake"
 
- sudo apt-get install --force-yes build-essential
+ sudo apt-get -y install build-essential
 # wget http://www.cmake.org/files/v3.5/cmake-3.5.2.tar.gz
 # tar xf cmake-3.5.2.tar.gz
 # cd cmake-3.5.2
@@ -11,10 +11,10 @@ echo "Installing cmake"
 # sudo checkinstall
 # cd ..
 
-sudo apt-get install --force-yes cmake
+sudo apt-get -y install cmake
 
 echo "Installing BOOST libraries"
-sudo apt-get install --force-yes libboost-dev libboost-program-options-dev
+sudo apt-get -y install libboost-dev libboost-program-options-dev
 
 #echo "Installing ZeroMQ"
 #sudo apt-get install -qq --force-yes libzmq5-dev
