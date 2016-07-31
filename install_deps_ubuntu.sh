41c2199e026803393e26f652273d2cf8396d3b44
ozamiatin <ozamiatin@mirantis.com>
2016-08-01 01:49:42 +0300

    Add more dependency to install

--- install_deps_ubuntu.sh
+++ install_deps_ubuntu.sh
@@ -1,5 +1,5 @@
 echo "Installing cmake"
-sudo apt-get -y install build-essential
+sudo apt-get -y install build-essential pkgconfig automake autoconf libtool
 sudo apt-get -y install cmake
 
 echo "Installing BOOST libraries"
