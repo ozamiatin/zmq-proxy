eb62eae1b6292f29473689922dbdebf417e5aca1
ozamiatin <ozamiatin@mirantis.com>
2016-07-31 20:47:15 +0300

    Add zmq install

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,9 +1,11 @@
-#echo "Installing BOOST libraries"
-#sudo apt-get install -qq --force-yes libboost-dev
-sudo apt-get install -qq --force-yes libboost-program-options-dev
-
 # echo "Installing cmake"
 # sudo apt-get install -qq --force-yes cmake
+
+#echo "Installing BOOST libraries"
+#sudo apt-get install -qq --force-yes libboost-dev libboost-program-options-dev
+
+echo "Installing ZeroMQ"
+sudo apt-get install -qq --force-yes libzmq5-dev
 
 mkdir build-Debug
 cd build-Debug
