e4d8d6092d28a2e31547fc70db900403cacb3a58
Oleksii Zamiatin <ozamiatin@mirantis.com>
2016-08-01 17:22:36 +0300

    Fixed zeromq build from source

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,10 +1,3 @@
-echo "Installing cmake"
-sudo apt-get -y install build-essential
-sudo apt-get -y install cmake
-
-echo "Installing BOOST libraries"
-sudo apt-get -y install libboost-dev libboost-program-options-dev
-
 mkdir build-Debug
 cd build-Debug
 
